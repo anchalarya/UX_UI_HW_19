27a4d70cca3cabfa56a305fe15e0bb2c99a20a8d
anchalarya <anchal.arya.22@gmail.com>
2022-07-24 02:41:51 -0400

    js testing

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("JS Script File loaded correctly!!");
+
+$(document).ready(function () {
+  // $(".logo").hover(function () {
+  //   $(this).attr('src', 'https://jpeg.org/images/jpeg-home.jpg');
+  // }, function () {
+  //   $(this).attr('src', 'https://jpeg.org/images/jpegsystems-home.jpg');
+  // })
+});  
