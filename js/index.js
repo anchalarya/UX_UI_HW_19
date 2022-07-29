0c9e663b64d2a7291523c9506ea7a7ae029a773f
anchalarya <anchal.arya.22@gmail.com>
2022-07-29 01:17:54 -0400

    Adding local JQuery and interactive elements

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
+"use strict";
+
 console.log("JS Script File loaded correctly!!");
 
 $(document).ready(function () {
-  // $(".logo").hover(function () {
-  //   $(this).attr('src', 'https://jpeg.org/images/jpeg-home.jpg');
-  // }, function () {
-  //   $(this).attr('src', 'https://jpeg.org/images/jpegsystems-home.jpg');
-  // })
+  $(".ui-design-item").mouseenter((evt) => {
+    // $(evt.currentTarget).css('opacity', '1')
+    // $(evt.currentTarget).css('animation', 'fadeIn ease-in 1')
+  }).mouseleave((evt) => {
+    // $(evt.currentTarget).css('opacity', '0.3')
+  })
+
+  $(".ui-design-tab")
+  .mouseenter((evt) => {
+    // $(evt.currentTarget.children).css('opacity', '0.3')
+  })
+  .mouseleave((evt) => {
+    console.log(evt)
+    // $(evt.currentTarget.children).css('opacity', '1')
+  })
+
 });  
+
