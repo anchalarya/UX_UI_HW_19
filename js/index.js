41b26bc15f9a0342521f88c873a1c785ddd115ab
anchalarya <anchal.arya.22@gmail.com>
2022-07-29 01:26:50 -0400

    adding event listener to JS file
    Requirement for HW 19

--- js/index.js
+++ js/index.js
@@ -4,20 +4,9 @@ console.log("JS Script File loaded correctly!!");
 
 $(document).ready(function () {
   $(".ui-design-item").mouseenter((evt) => {
-    // $(evt.currentTarget).css('opacity', '1')
-    // $(evt.currentTarget).css('animation', 'fadeIn ease-in 1')
+    $(evt.currentTarget).css('transform', 'scale(1.1)')    
   }).mouseleave((evt) => {
-    // $(evt.currentTarget).css('opacity', '0.3')
+    $(evt.currentTarget).css('transform', 'scale(1)')    
   })
-
-  $(".ui-design-tab")
-  .mouseenter((evt) => {
-    // $(evt.currentTarget.children).css('opacity', '0.3')
-  })
-  .mouseleave((evt) => {
-    console.log(evt)
-    // $(evt.currentTarget.children).css('opacity', '1')
-  })
-
 });  
 
